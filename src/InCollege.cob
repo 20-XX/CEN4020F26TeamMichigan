71264dacff667220bf0131a4663e2e671bc0f276
Philippe <ppaulkhs@gmail.com>
2026-02-03 16:08:25 +0000

    Implemented working storage profile logic.

--- src/InCollege.cob
+++ src/InCollege.cob
@@ -90,6 +90,28 @@
 
        01 WS-YEAR-INPUT           PIC X(4).
 
+       01 WS-PROFILE-RECORD
+           05 WS-PR-USERNAME
+           05 WS-PR-FIRST-NAME
+           05 WS-PR-LAST-NAME
+           05 WS-PR-UNIVERSITY
+           05 WS-PR-MAJOR
+           05 WS-PR-GRAD-YEAR
+           05 WS-PR-ABOUT
+           05 WS-PR-EXP-COUNT
+           05 WS-PR-EXPERIENCE OCCURS 3 TIMES.
+               10 WS-PR-EXP-TITLE   PIC X(30).
+               10 WS-PR-EXP-COMPANY PIC X(30).
+               10 WS-PR-EXP-DATES   PIC X(20).
+               10 WS-PR-EXP-DESC    PIC X(100).
+
+           05 WS-PR-EDU-COUNT       PIC 9.
+           05 WS-PR-EDUCATION OCCURS 3 TIMES.
+               WS-10 PR-EDU-DEGREE  PIC X(30).
+               WS-10 PR-EDU-SCHOOL  PIC X(40).
+               WS-10 PR-EDU-YEARS   PIC X(15).
+
+
 
        PROCEDURE DIVISION.
        MAIN.
@@ -446,6 +468,8 @@
 
            EXIT PARAGRAPH.
        SAVE-PROFILE.
+           MOVE PROFILE-RECORD TO WS-PROFILE-RECORD
+
             MOVE "N" TO PROFILE-FOUND
             MOVE "N" TO PROFILE-EOF
 
